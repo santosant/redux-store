37a2191ad50ecf8979d003713d207ee9f6daad56
Antonio Santos <hiroto.anderson@gmail.com>
2022-01-11 18:29:03 -0300

    refactor: produce

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -8,22 +8,23 @@ const INITIAL_STATE: ICartState = {
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "ADD_PRODUCT_TO_CART": {
-      const { product } = action.payload;
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART": {
+        const { product } = action.payload;
 
-      return produce(state, (draft) => {
         draft.items.push({
           product,
           quantity: 1,
         });
-      });
-    }
 
-    default: {
-      return state;
+        break;
+      }
+      default: {
+        return state;
+      }
     }
-  }
+  });
 };
 
 export default cart;
