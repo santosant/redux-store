00464d0fa4e5a7913f000d93bdf3cbfc9371418b
Antonio Santos <hiroto.anderson@gmail.com>
2022-01-18 19:08:25 -0300

    Dividing actions at actions.ts

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,22 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, takeLatest, select } from "redux-saga/effects";
+import { addProductToCartSuccess } from "./actions";
+import { IState } from "../..";
 
-function checkProductStock() {
-  console.log("Adicionou ao carrinho");
+type CheckProductStockRequest = ReturnType<typeof addProductToCartSuccess>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
 }
 
-export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
+export default all([
+  takeLatest("ADD_PRODUCT_TO_CART_REQUEST", checkProductStock),
+]);
